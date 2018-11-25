8b5311bfa3fb9ec51afd367050f7320c2bf91581
Gamebug <gamebug@gamebugs-imac.local>
2018-11-25 17:44:49 +0530

    Initial Commit

---
+++ Notepad2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notepad2
+//
+//  Created by Gamebug on 25/11/18.
+//  Copyright © 2018 Gamebug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
